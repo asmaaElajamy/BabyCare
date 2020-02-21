25c7990c4d056a29b650c339863c70bbfcd35f23
AhmedHawam2798 <ahmed271998271998>
2020-02-22 00:18:24 +0200

    loginvalidation

--- app/src/main/java/com/example/babyinformation/login.java
+++ app/src/main/java/com/example/babyinformation/login.java
@@ -6,29 +6,18 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.regex.Pattern;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class login extends AppCompatActivity {
-    private static final Pattern PASSWORD_PATTERN =
-            Pattern.compile("^" +
-                    //"(?=.*[0-9])" +         //at least 1 digit
-                    //"(?=.*[a-z])" +         //at least 1 lower case letter
-                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
-                    "(?=.*[a-zA-Z])" +      //any letter
-                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-                    "(?=\\S+$)" +           //no white spaces
-                    ".{4,}" +               //at least 4 characters
-                    "$");
+
     private EditText Email;
     private EditText Password;
-    private Button Login;
-    private Button Info;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,62 +26,44 @@ public class login extends AppCompatActivity {
 
         Email = (EditText) findViewById(R.id.etEmail);
         Password = (EditText) findViewById(R.id.etPassword);
-        Login = (Button) findViewById(R.id.loginbtn);
+        btnLogin = (Button) findViewById(R.id.loginbtn);
 
-        Login.setOnClickListener(new View.OnClickListener() {
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Login();
+                loginValidation();
             }
+
         });
+
+
     }
 
-    public void Login(){
-        Intent intent = new Intent(login.this, SecondActivity.class);
-        startActivity(intent);
-    }
 
-    private boolean validateEmail() {
-        String emailInput = Email.getText().toString().trim();
+    private void loginValidation () {
+        if (Email.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Email is Required", Toast.LENGTH_SHORT).show();
+            Email.setError("name is required");
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(Email.getText().toString().trim()).matches()) {
+            Email.setError("email Is Invalid");
+            Toast.makeText(this, "Email must be in this way 'example@example.com' ", Toast.LENGTH_LONG).show();
 
-        if (emailInput.isEmpty()) {
-            Email.setError("Field can't be empty");
-            return false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
-            Email.setError("Please enter a valid email address");
-            return false;
+        }
+        if (Password.getText().toString().length() < 6) {
+            Password.setError("Password Is Invalid");
+            Toast.makeText(this, "Password Must be more 6 character", Toast.LENGTH_LONG).show();
         } else {
-            Email.setError(null);
-            return true;
+            Intent login = new Intent(this, SecondActivity.class);
+            startActivity(login);
+            finish();
         }
+
+
     }
 
 
-    private boolean validatePassword() {
-        String passwordInput = Password.getText().toString().trim();
 
-        if (passwordInput.isEmpty()) {
-            Password.setError("Field can't be empty");
-            return false;
-        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
-            Password.setError("Password too weak");
-            return false;
-        } else {
-            Password.setError(null);
-            return true;
-        }
-    }
-
-    public void confirmInput(View v) {
-        if (!validateEmail() |!validatePassword()) {
-            return;
-        }
-
-        String input = "Email: " + Email.getText().toString();
-        input += "\n";
-        input += "Password: " + Password.getText().toString();
-
-        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
-    }
 
 }
